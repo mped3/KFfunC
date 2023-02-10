bb1e5a2de4905e31d8a0701dacdc8d7e53a4cc5e
unknown <pedifordm@icloud.com>
2023-02-10 05:37:40 -0500

    first commit

---
+++ kframes.js
@@ -0,0 +1,24 @@
+/*const fallDown =  () => {
+    let mario = document.querySelector('sm')
+    mario.classList.toggle('falling')
+} */
+
+let favDay = {
+    day:"Friday",
+    activity: "Art Museum",
+    homework: false,
+    timeClassEnds: 4
+
+}
+console.log(favDay)
+
+let result = words('YERRRR')
+function words(message) {
+    console.log(message);
+}
+console.log(result)
+
+const cars = ('BMW','BENZ','LAMBO')
+console.log(cars[2])
+cars.shift('BMW')
+cars.unshift("BENTLEY")
